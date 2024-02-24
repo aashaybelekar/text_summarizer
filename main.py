81f04ee6d19b97c14b199893b8adebc55a5b0df4
aashaybelekar <aashaybelekar22@gmail.com>
2024-02-24 23:08:12 +0530

    evaluation stage added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTraini
 from text_summarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from text_summarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from text_summarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from text_summarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from text_summarizer.logging import logger
 
 
@@ -39,6 +40,16 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
